a077b4a4e8042f90892a7432acd1f7f13ca9b34c
ErikBorisov <Erik.borisov.2099@gmail.com>
2022-07-07 23:15:10 +0400

    Added new class(BubbleSort) and modify Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        int[] array = {10, 5, 3, 1, 24, 12};
+        int len = array.length;
+        BubbleSort.bubbleSort(array, len);
 
+        for (int i = 0; i < len; ++i) {
+            System.out.print(array[i] + " ");
+        }
     }
 }
